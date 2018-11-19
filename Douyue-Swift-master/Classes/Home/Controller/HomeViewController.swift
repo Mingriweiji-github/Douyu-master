ff07d8e30447748111af087d1f7b077131512d76
KM <18610929200@163.com>
2018-11-19 14:41:50 +0800

    int project

---
+++ Douyue-Swift-master/Classes/Home/Controller/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Douyue-Swift-master
+//
+//  Created by Roc01 on 2018/11/19.
+//  Copyright © 2018 Roc.iMac01. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Douyue-Swift-master/Classes/Main/Controller/MainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarController.swift
+//  Douyue-Swift-master
+//
+//  Created by Roc01 on 2018/11/19.
+//  Copyright © 2018 Roc.iMac01. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addChildVCs(storyName: "Home")
+        addChildVCs(storyName: "Follow")
+        addChildVCs(storyName: "Live")
+        addChildVCs(storyName: "Profile")
+    }
+    
+    private func addChildVCs(storyName: String) {
+//        guard let vc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController() else { return }
+        let vc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        
+        addChildViewController(vc)
+    }
+}
+
+
